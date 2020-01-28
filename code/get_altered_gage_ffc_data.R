f1d83707c276d0b24d1315a7584ed42b4f3e2686
Ryan Peek <rapeek@ucdavis.edu>
2020-01-28 11:05:51 -0800

    update code to include list names

--- code/get_altered_gage_ffc_data.R
+++ code/get_altered_gage_ffc_data.R
@@ -51,10 +51,10 @@ get_ffc_eval <- possibly(evaluate_gage_alteration, otherwise=NA_real_)
 
 # this works for a list of gages and adds list cols, no plots
 tic(msg = "Starting Download") # time start
-g100 <- usgs_list %>% 
-  slice(1:100) %>%  # pick only a subset from gage list
+g300 <- usgs_list %>% 
+  slice(201:300) %>%  # pick only a subset from gage list
   split(.$gage_id) %>% # make into named list
-  map(., ~{pluck(.x$gage_id)}) #%>% # pull only the gage ID out
+  map(., ~{pluck(.x$gage_id)}) %>% # pull only the gage ID out
   furrr::future_imap(., 
                      ~get_ffc_eval(gage_id = .x, 
                                    token = ffctoken, 
@@ -88,41 +88,47 @@ f_remove_empty <- function(x){
 gAll_filt <- f_remove_empty(gAll)
 names(gAll_filt)
 
+g100f <- f_remove_empty(g100)
+
+g200f <- f_remove_empty(g200)
+
+g300f <- f_remove_empty(g300)
+
 # SAVE OUT FULL LIST HERE
 
 # bind together multiple runs into one file (can save and read in iteratively)
-# usgs_ffc_alt <- append(g200, g300, g500)
-usgs_ffc_alt <- gAll # rename
+usgs_ffc_alt <- append(x = g100f, values=c(g200f, g300f))
+#usgs_ffc_alt <- gAll # rename
 
 # save
-save(usgs_ffc_alt, file = "data_output/usgs_altered_ffc.rda")
+save(usgs_ffc_alt, file = "data_output/usgs_altered_ffc_list.rda")
 
 
 # 03. GET PERCENTILES AND FLATTEN TO DF -------------------------------
 
 # Get all the Percentiles and put into one single large dataframe
-gAll_percentiles <- map(gAll_filt, ~{bind_rows(.x$ffc_percentiles,  .x$predicted_percentiles)}) %>% bind_rows(., .id = "list_id") # since gage_id already in df (just a double check here)
+g_alt_percentiles <- map(usgs_ffc_alt, ~{bind_rows(.x$ffc_percentiles,  .x$predicted_percentiles)}) %>% bind_rows(., .id = "list_id") # since gage_id already in df (just a double check here)
 
 # save 
-save(gAll_percentiles, file = "data_output/usgs_altered_ffc_percentiles.rda")
-write_csv(gAll_percentiles, path="data_output/usgs_altered_ffc_percentiles.csv")
+save(g_alt_percentiles, file = "data_output/usgs_altered_ffc_percentiles.rda")
+write_csv(g_alt_percentiles, path="data_output/usgs_altered_ffc_percentiles.csv")
 
 # 04. GET ALL FFM AND FLATTEN TO DF -----------------------------------
 
 # Get all the FFM and put into one single large dataframe
-gAll_ffc <- map(gAll_filt, ~.x$ffc_results) %>% 
+g_alt_ffc <- map(usgs_ffc_alt, ~.x$ffc_results) %>% 
   bind_rows(., .id="gage_id") # gage_id not in this df
 
 # save 
-save(gAll_ffc, file = "data_output/usgs_altered_ffc_metrics.rda")
-write_csv(gAll_ffc, path="data_output/usgs_altered_ffc_metrics.csv")
+save(g_alt_ffc, file = "data_output/usgs_altered_ffc_metrics.rda")
+write_csv(g_alt_ffc, path="data_output/usgs_altered_ffc_metrics.csv")
 
 
 # 05. GET ALL ALTERATION STATUS AND FLATTEN TO DF ---------------------
 
-gAll_alt <- map(gAll_filt, ~.x$alteration) %>% 
+g_alt_alt <- map(usgs_ffc_alt, ~.x$alteration) %>% 
   bind_rows(., .id="list_id") # since gage_id already in df
 
 # save 
-save(gAll_alt, file = "data_output/usgs_altered_ffc_alteration.rda")
-write_csv(gAll_alt, path="data_output/usgs_altered_ffc_alteration.csv")
+save(g_alt_alt, file = "data_output/usgs_altered_ffc_alteration.rda")
+write_csv(g_alt_alt, path="data_output/usgs_altered_ffc_alteration.csv")
