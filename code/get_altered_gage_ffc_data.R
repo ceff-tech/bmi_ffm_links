b0b1146decc368d8519719cc9e48ae9e73f903f4
Ryan Peek <rapeek@ucdavis.edu>
2020-01-28 10:15:58 -0800

    update code to get all the data for ffm for altered gages

--- code/get_altered_gage_ffc_data.R
+++ code/get_altered_gage_ffc_data.R
@@ -1,16 +1,19 @@
 # Get Altered Gage FFM 
 
-
 # Libraries ---------------------------------------------------------------
 
-library(tidyverse)
-library(tictoc)
-#library(tidylog)
-
+# MAIN
 #devtools::install_github('ceff-tech/ffc_api_client/ffcAPIClient')
 library(ffcAPIClient)
-#options(scipen = 999) # turn of scientific notation
+
+# set/get the token for using the FFC
 ffctoken <- set_token(Sys.getenv("EFLOWS_TOKEN", "")) 
+
+# SUPPPORTING
+library(tidyverse) # yes
+library(tictoc) # timing stuff
+library(furrr) # parallel processing for mapping functions/loops (purrr)
+#library(tidylog) # good for logging what happens
 
 # Get Gages ---------------------------------------------------------------
 
@@ -23,139 +26,101 @@ usgs_list <- usgs_list %>%
   select(gage_id, ID:LONGITUDE, FLOW_YRS_POST_1950:REF_END_YEAR) 
 
 # look at reference?
-table(usgs_list$FINAL_REFERENCE) # 30=Y here, included in reference data but pre-regulation?
+table(usgs_list$FINAL_REFERENCE) # 30=Y here, included in reference data but pre-regulation, so some stretch of years is a mix of ref/altered
 
-# Setup Error Functions for ffcAPI ----------------------------------------
+# Test with a Single Gage -------------------------------------------------
 
-# this was a workaround to return NAs if there were errors
-# get_ffc <- possibly(get_ffc_results_for_usgs_gage, otherwise = NA_real_)
-# tst <- get_ffc(10253080) # should give NA
-# get_ffc_df <- possibly(get_results_as_df, otherwise = NA_real_)
-# get_ffc_percentiles <- possibly(get_percentiles, otherwise = NA_real_)
-get_ffc_eval <- possibly(evaluate_gage_alteration, otherwise=NA_real_)
-
-# Gage Tests -------------------------------------------------------
-
-# this works for a single gage and shows plots
+# this works for a single gage and doesn't show plots
 g1 <- usgs_list %>% 
   slice(5) %>% pull(gage_id) %>% 
-  evaluate_gage_alteration(., ffctoken, plot_results = TRUE)
+  evaluate_gage_alteration(gage_id = ., 
+                           token = ffctoken, 
+                           plot_results = FALSE)  
+
+# here we get a list of 5 df
+names(g1)
+
+# pull just one out
+g1$alteration
+
+
+# 01. DOWNLOAD Multiple Gages IN LIST ------------------------------
+
+# Error wrapper to return NA if the gage data doesn't exist/not enough data
+get_ffc_eval <- possibly(evaluate_gage_alteration, otherwise=NA_real_)
 
 # this works for a list of gages and adds list cols, no plots
-g2 <- usgs_list %>% 
-  slice(1:5) %>% 
-  select(gage_id, NHDV2_COMID) %>% 
-  mutate(
-    alt_eval = furrr::future_imap(
-      gage_id, ~evaluate_gage_alteration(.x, ffctoken, 
-                                         plot_results = FALSE),
-      .progress = TRUE)) 
-
-
-# ALL ---------------------------------------------------------------------
-
-tic()
-# get ffc and percentiles (observed)
-g500 <- usgs_list %>% 
-  slice(301:500) %>% 
-  select(gage_id, NHDV1_COMID) %>% split(.$gage_id) %>% 
+tic(msg = "Starting Download") # time start
+gAll <- usgs_list %>% 
+  slice(1:5) %>%  # pick only a subset from gage list
+  split(.$gage_id) %>% # make into named list
+  map(., ~{pluck(.x$gage_id)}) %>% # pull only the gage ID out
   furrr::future_imap(., 
-      ~get_ffc_eval(.x$gage_id, ffctoken, plot_results = FALSE),
-      .progress = TRUE)
-beepr::beep(2)
+                     ~get_ffc_eval(gage_id = .x, 
+                                   token = ffctoken, 
+                                   plot_results = FALSE), 
+                     .progress = TRUE) 
+beepr::beep(2) # something fun to let you know it's done
 toc()
 
-# bind and save
-usgs_ffc_data <- append(g200, g300, g500)
+# Make sure the list is named
+names(gAll)
 
-# save out
-save(usgs_ffc_data, file = "data_output/usgs_altered_ffc_observed.rda")
+# 02. CLEAN NAs ----------------------------------------------------
 
+# because some gages return NA, we have a list that contains some NA
+# and some actual data. This function helps us remove the NAs in the list.
 
-# get preds
-gPreds <- usgs_list %>% 
-  slice(1:10) %>% 
-  select(NHDV2_COMID, gage_id) %>% split(.$NHDV2_COMID) %>% 
-  map(., ~get_predicted_flow_metrics(.x$NHDV2_COMID))
+# Function to remove empty/NAs from the list
+f_remove_empty <- function(x){
+  if(is.list(x)) {
+    x %>%
+      purrr::discard(rlang::is_na) %>%
+      purrr::map(remove_empty)
+  } else {
+    x
+  }
+}
 
+# remove NAs
+gAll_filt <- f_remove_empty(gAll)
+names(gAll_filt)
 
-# pwalk to get the alteration
-gPercentiles <- map_df(gAll, ~.x["percentiles"])
+# SAVE OUT FULL LIST HERE
 
-#
-#pwalk(list(gAll[[.x]][[2]], gAll[[..2]], ~assess_alteration(..1, ..2, ..3, ..4)))
+# bind together multiple runs into one file (can save and read in iteratively)
+# usgs_ffc_alt <- append(g200, g300, g500)
+usgs_ffc_alt <- gAll # rename
 
-# this works
-#assess_alteration(percentiles = gAll[[4]][[2]], ffc_values = gAll[[4]][[1]], predictions = gPreds[[4]], comid = names(gPreds)[4])
-
-# Load FFC Data -----------------------------------------------------------
-
-# old version
-#load("data_output/usgs_altered_ffc_observed.rda")
-
-# Run FFC: Get FFC Results as DF ------------------------------------------
-
-# use flow calculator to pull FFM for each gage, this works as raw option
-tic()
-usgs_ffc_dat_100 <- usgs_list %>% 
-  slice(1:7) %>% # pick a few rows to test
-  #mutate(alt_eval = furrr::future_imap(gage_id, ~get_ffc_eval(.x, ffctoken))) %>% 
-  mutate(ffc_df = furrr::future_imap(gage_id, ~get_ffc(.x))) %>%
-  mutate(ffc_df = furrr::future_imap(ffc_df, ~get_ffc_df(.x))) #%>% 
-  #mutate(ffc_percentiles = furrr::future_imap(ffc_df, ~get_ffc_percentiles(.x))) %>%
-  #unnest(cols = c(ffc_percentiles)) # pull out percentiles
-beepr::beep(2)
-toc()
-
-# bind and save
-usgs_ffc_data <- bind_rows(usgs_ffc_dat_100, usgs_ffc_dat_200, usgs_ffc_dat_300, usgs_ffc_dat_400, usgs_ffc_dat_500, usgs_ffc_dat_600, usgs_ffc_dat_700, usgs_ffc_dat_800)
-
-# save out
-save(usgs_ffc_data, file = "data_output/usgs_altered_ffc_observed.rda")
-
-# check no of gages
-usgs_ffc_data %>% distinct(gage_id) %>% tally()
-usgs_ffc_data %>% group_by(Metric) %>% tally() %>% View()# so over half are NA
-
-# NOTES:
-## if unnesting by ffc_df, will get ffc metrics as cols for each year of gage (years as rows)
-# To add column inside of list-column dataframe:
-## mutate(ffc_percentiles = map_depth(ffc_percentiles, 1, ~mutate(.x, data_source="ffc_obs")))
-## convert to dataframe (not listcol)
-## unnest(cols=c(ffc_percentiles))
-
-# SAVE SAVE SAVE
-save(usgs_ffc_dat, file = "data_output/usgs_altered_ffc_observed.rda")
+# save
+save(usgs_ffc_alt, file = "data_output/usgs_altered_ffc.rda")
 
 
-# Get Predicted Metrics ---------------------------------------------------
+# 03. GET PERCENTILES AND FLATTEN TO DF -------------------------------
 
-# All predicted metrics
-tic(msg="Getting predicted percentiles")
+# Get all the Percentiles and put into one single large dataframe
+gAll_percentiles <- map(gAll_filt, ~{bind_rows(.x$ffc_percentiles,  .x$predicted_percentiles)}) %>% bind_rows(., .id = "list_id") # since gage_id already in df (just a double check here)
 
-usgs_ffc_pred <- usgs_list %>% 
-  # run in parallel with furrr
-  mutate(ffc_pred = furrr::future_imap(NHDV2_COMID, ~get_predicted_flow_metrics(.x))) %>% 
-  unnest_longer(col=ffc_pred)
-beepr::beep(2)
-toc()
+# save 
+save(gAll_percentiles, file = "data_output/usgs_altered_ffc_percentiles.rda")
+write_csv(gAll_percentiles, path="data_output/usgs_altered_ffc_percentiles.csv")
 
-# SAVE SAVE SAVE
-save(usgs_ffc_pred, file = "data_output/usgs_altered_ffc_predicted.rda")
+# 04. GET ALL FFM AND FLATTEN TO DF -----------------------------------
 
-# Run FFC: Evaluate Gage Alteration ---------------------------------------
+# Get all the FFM and put into one single large dataframe
+gAll_ffc <- map(gAll_filt, ~.x$ffc_results) %>% 
+  bind_rows(., .id="gage_id") # gage_id not in this df
 
-# try using FFC with evaluate_gage_alteration
-eval_alt <- function(x) {possibly(evaluate_gage_alteration(gage_id = x, token = Sys.getenv("EFLOWS_TOKEN", "")), NA_real_)}
-
-tst_eval <- eval_alt(10253080)
-#tst_eval <- ffcAPIClient::evaluate_gage_alteration(gage_id = 10253080, token = Sys.getenv("EFLOWS_TOKEN", ""))
+# save 
+save(gAll_ffc, file = "data_output/usgs_altered_ffc_metrics.rda")
+write_csv(gAll_ffc, path="data_output/usgs_altered_ffc_metrics.csv")
 
 
-usgs_ffc_eval <- usgs_list %>% 
-  slice(1:2) %>% purrr::transpose(.names = usgs_list$gage_id)
-  map(., ~evaluate_gage_alteration(.x, token = Sys.getenv("EFLOWS_TOKEN", "")))
+# 05. GET ALL ALTERATION STATUS AND FLATTEN TO DF ---------------------
 
+gAll_alt <- map(gAll_filt, ~.x$alteration) %>% 
+  bind_rows(., .id="list_id") # since gage_id already in df
 
-# add col inside dataframe
-# map2_df(dat2, stocks, ~update_list(.x, stock = .y))
+# save 
+save(gAll_alt, file = "data_output/usgs_altered_ffc_alteration.rda")
+write_csv(gAll_alt, path="data_output/usgs_altered_ffc_alteration.csv")
