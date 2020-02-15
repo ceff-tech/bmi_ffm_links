8fdaa2f5206ac5c4c2b532e3d5fa866cc99a22a6
Ryan Peek <rapeek@ucdavis.edu>
2020-02-14 17:03:23 -0800

    update all altered gages for FFC calc last last time??

--- code/04a_get_altered_gage_ffc_data.R
+++ code/04a_get_altered_gage_ffc_data.R
@@ -3,7 +3,7 @@
 # Libraries ---------------------------------------------------------------
 
 # MAIN
-devtools::install_github('ceff-tech/ffc_api_client/ffcAPIClient')
+#devtools::install_github('ceff-tech/ffc_api_client/ffcAPIClient')
 library(ffcAPIClient)
 
 # set/get the token for using the FFC
@@ -15,63 +15,12 @@ library(tictoc) # timing stuff
 library(furrr) # parallel processing for mapping functions/loops (purrr)
 #library(tidylog) # good for logging what happens
 
-# Get Gages ---------------------------------------------------------------
-
-# read in altered gage list
-usgs_list <- read_csv("data/FinalAlteredList.csv")
-
-# clean up: drop the "T" from the ID, drop cols
-usgs_list <- usgs_list %>% 
-  mutate(gage_id=as.integer(gsub("^T",replacement = "", ID))) %>% 
-  select(gage_id, ID:LONGITUDE, FLOW_YRS_POST_1950:REF_END_YEAR) 
-
-# look at reference?
-table(usgs_list$FINAL_REFERENCE) # 30=Y here, included in reference data but pre-regulation, so some stretch of years is a mix of ref/altered
-
-# Test with a Single Gage -------------------------------------------------
-
-# this works for a single gage and doesn't show plots
-g1 <- usgs_list %>% 
-  slice(5) %>% pull(gage_id) %>% 
-  evaluate_gage_alteration(gage_id = ., 
-                           token = ffctoken, 
-                           plot_results = FALSE)  
-
-# here we get a list of 5 df
-names(g1)
-
-# look at just one
-glimpse(g1$alteration)
-
-
-# 01. DOWNLOAD Multiple Gages IN LIST ------------------------------
+# Functions For Running Things --------------------------------------------
 
 # Error wrapper to return NA if the gage data doesn't exist/not enough data
 get_ffc_eval <- possibly(evaluate_gage_alteration, otherwise=NA_real_)
 
-# this works for a list of gages and adds list cols, no plots
-tic(msg = "Finished Getting Data") # time start
-g800 <- usgs_list %>% 
-  slice(701:814) %>%  # pick only a subset from gage list
-  split(.$gage_id) %>% # make into named list
-  map(., ~{pluck(.x$gage_id)}) %>% # pull only the gage ID out
-  furrr::future_imap(., 
-                     ~get_ffc_eval(gage_id = .x, 
-                                   token = ffctoken, 
-                                   plot_results = FALSE), 
-                     .progress = TRUE) 
-beepr::beep(2) # something fun to let you know it's done
-toc()
-
-# Make sure the list is named
-#names(gAll)
-
-# 02. CLEAN NAs ----------------------------------------------------
-
-# because some gages return NA, we have a list that contains some NA
-# and some actual data. This function helps us remove the NAs in the list.
-
-# Function to remove empty/NAs from the list
+# Some gages return NA due to lack of data need a function to remove these NAs from a list
 f_remove_empty <- function(x){
   if(is.list(x)) {
     x %>%
@@ -82,39 +31,113 @@ f_remove_empty <- function(x){
   }
 }
 
-# remove NAs
-#gAll_filt <- f_remove_empty(gAll)
-#names(gAll_filt)
+# Get Gages ---------------------------------------------------------------
+
+# read in altered gage list
+usgs_list <- read_csv("data/usgs/usgs_gages_final_altered_list.csv")
+
+# clean up: drop the "T" from the ID, drop cols
+usgs_list <- usgs_list %>% 
+  mutate(gage_id=as.integer(gsub("^T",replacement = "", ID))) %>% 
+  select(gage_id, ID:LONGITUDE, FLOW_YRS_POST_1950:REF_END_YEAR) 
+
+# look at reference?
+table(usgs_list$FINAL_REFERENCE) # 30=Y here, 
+# the Y are included in reference data but pre-regulation, 
+# so some stretch of years is a mix of ref/altered
+
+# Test with a Single Gage -------------------------------------------------
+
+# this works for a single gage and doesn't show plots
+# g1 <- usgs_list %>% 
+#   slice(5) %>% 
+#   select(gage_id, NHDV2_COMID) %>% 
+#   evaluate_gage_alteration(gage_id = .$gage_id,  
+#                            comid = .$NHDV2_COMID,
+#                            token = ffctoken, 
+#                            plot_results = FALSE)   
+
+# here we get a list of 5 df
+# names(g1)
+# glimpse(g1$alteration)
+
+# Test with multi Gages w Purrr -------------------------------------------------------
+
+# try in loop fashion
+g2 <- usgs_list %>% 
+  slice(1:5) %>%  # pick only a subset from gage list
+  split(.$gage_id) %>% # make into named list
+  map(., ~{select(.x, gage_id, NHDV2_COMID)}) %>% # pull gage ID out
+  map(., ~get_ffc_eval(gage_id = .x$gage_id, 
+                       token = ffctoken,
+                       comid = .x$NHDV2_COMID,
+                       force_comid_lookup = FALSE,
+                       plot_results = FALSE))
+  
+# here we get a list of the gages
+names(g2)
+
+# look at first gage in list
+glimpse(g2[[1]]$alteration)
+
+# drop/check for NAs
+length(g2)
+g2_f <- f_remove_empty(g2) # so dropped 2
+length(g2_f)
+
+# rm
+rm(g2, g2_f)
+
+# 01. DOWNLOAD Multiple Gages IN LIST ------------------------------
+
+# RUN
+tic(msg = "Finished Getting Data") # time start
+g800 <- usgs_list %>% 
+  slice(601:814) %>%  # pick a subset of rows from gage list
+  split(.$gage_id) %>% # make into named list
+  map(., ~{select(.x, gage_id, NHDV2_COMID)}) %>% # pull gage ID out
+  furrr::future_imap(., 
+                     ~get_ffc_eval(gage_id = .x$gage_id, 
+                                   token = ffctoken,
+                                   comid = .x$NHDV2_COMID,
+                                   force_comid_lookup = FALSE,
+                                   plot_results = FALSE), 
+                     .progress = TRUE) 
+beepr::beep(2) # something fun to let you know it's done
+toc()
+
+# 02. CLEAN NAs ----------------------------------------------------
 
 # completed
 #g100f <- f_remove_empty(g100)
 #g200f <- f_remove_empty(g200)
-#g300f <- f_remove_empty(g300)
-#g400f <- f_remove_empty(g400)
-#g500f <- f_remove_empty(g500)
-#g550f <- f_remove_empty(g550)
-#g600f <- f_remove_empty(g600)
-#g700f <- f_remove_empty(g700)
+g400f <- f_remove_empty(g400)
+g600f <- f_remove_empty(g600)
 g800f <- f_remove_empty(g800)
 
-# SAVE OUT FULL LIST HERE
+# 03. SAVE OUT ------------------------------------------------------------
 
-# FIRST TIME: bind together multiple runs into one file
-# usgs_ffc_alt <- append(x = g100f, values=c(g200f, g300f)) # first time
-
-# Subsequently
-load("data_output/usgs_altered_ffc_list.rda") # load existing dataset
-usgs_ffc_alt <- append(x = usgs_ffc_alt, values=c(g800f)) # merge
+# FIRST TIME THROUGH 
+# bind together multiple runs into one file
+usgs_ffc_alt <- append(x = g400f, values=c(g600f, g800f)) 
 
 # save
 save(usgs_ffc_alt, file = "data_output/usgs_altered_ffc_list.rda")
 
-# so have 84% of the data
+# If saving and rerunning/adding things subsequently
+#load("data_output/usgs_altered_ffc_list.rda") # load existing dataset
+
+# update/add more data
+#usgs_ffc_alt <- append(x = usgs_ffc_alt, values=c(g800f))
+
+# so 576/814 = 70% of gages we could get data for
 
 # 03. GET PERCENTILES AND FLATTEN TO DF -------------------------------
 
 # Get all the Percentiles and put into one single large dataframe
 g_alt_percentiles <- map(usgs_ffc_alt, ~{bind_rows(.x$ffc_percentiles,  .x$predicted_percentiles)}) %>% bind_rows(., .id = "list_id") # since gage_id already in df (just a double check here)
+
+# g_alt_percentiles %>% distinct(gage_id)
 
 # save 
 save(g_alt_percentiles, file = "data_output/usgs_altered_ffc_percentiles.rda")
@@ -126,6 +149,8 @@ write_csv(g_alt_percentiles, path="data_output/usgs_altered_ffc_percentiles.csv"
 g_alt_ffc <- map(usgs_ffc_alt, ~.x$ffc_results) %>% 
   bind_rows(., .id="gage_id") # gage_id not in this df
 
+# g_alt_ffc %>% distinct(gage_id)
+
 # save 
 save(g_alt_ffc, file = "data_output/usgs_altered_ffc_metrics.rda")
 write_csv(g_alt_ffc, path="data_output/usgs_altered_ffc_metrics.csv")
@@ -133,9 +158,21 @@ write_csv(g_alt_ffc, path="data_output/usgs_altered_ffc_metrics.csv")
 
 # 05. GET ALL ALTERATION STATUS AND FLATTEN TO DF ---------------------
 
-g_alt_alt <- map(usgs_ffc_alt, ~.x$alteration) %>% 
+g_alt_alt <- map(g2_f, ~unnest(.x$alteration)) #%>%
   bind_rows(., .id="list_id") # since gage_id already in df
 
 # save 
 save(g_alt_alt, file = "data_output/usgs_altered_ffc_alteration.rda")
 write_csv(g_alt_alt, path="data_output/usgs_altered_ffc_alteration.csv")
+
+
+# 06. GET WYT PERCENTILES -------------------------------------------------
+
+# Get all the Percentiles and put into one single large dataframe
+g_alt_wyt_percentiles <- map(usgs_ffc_alt, ~.x$predicted_wyt_percentiles) %>%
+  bind_rows(., .id = "gage_id") # since gage_id already in df 
+g_alt_wyt_percentiles %>% distinct(gage_id)
+
+# save 
+save(g_alt_wyt_percentiles, file = "data_output/usgs_altered_ffc_wytpercentiles.rda")
+write_csv(g_alt_wyt_percentiles, path="data_output/usgs_altered_ffc_wytpercentiles.csv")
