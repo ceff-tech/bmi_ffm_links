699001352c334d0098586e667d260506f1a6f6cc
Ryan Peek <rapeek@ucdavis.edu>
2020-12-15 17:33:38 -0800

    update map

--- code/03_map_final_selected_sites.R
+++ code/03_map_final_selected_sites.R
@@ -1,4 +1,4 @@
-# 04 Generate Final Selected Sites/Data
+# 0 Generate Final Selected Sites/Data
 ## R. Peek
 ## Look at final output
 
@@ -8,6 +8,7 @@
 library(tidyverse)
 library(tidylog)
 library(sf)
+library(glue)
 library(mapview)
 library(lubridate)
 library(tmap)
@@ -18,39 +19,17 @@ library(tmaptools)
 # FISH REGIONS
 ca_sp_regions <- read_sf("data/spatial/umbrella_sp_regions.shp", as_tibble = T)
 
-# BMI SITES
-load("data_output/03_selected_final_bmi_stations_dat_all_gages.rda")
+# All BMI data
+load("data_output/02c_selected_final_bmi_dat_all.rda")
 
-# BMI
-sel_bmi_gages_csci <- readRDS("data_output/03_selected_bmi_h12_all_gages_csci.rds") # sel_bmi_gages w csci scores
+# Streamlines
+load("data_output/02b_sel_gage_mainstems_all.rda")
 
-sel_gages_bmi <- readRDS("data_output/03_selected_usgs_h12_all_gages.rds")
-sel_h12_bmi <- readRDS("data_output/03_selected_h12_all_gages.rds")
-load("data_output/03_selected_nhd_mainstems_gages.rda")
-
-
-# Clean Data a Bit --------------------------------------------------------
-
-
-# not selected bmi
-bmi_not_selected_v2 <- sel_bmi_gages_csci %>% filter(!StationCode %in% sel_bmi_coms_final_v2$StationCode) # n=303
-
-# get all gages selected (n=326)
-gages_selected_v2 <- sel_gages_bmi %>% 
-  filter(site_id %in% sel_bmi_coms_final_v2$site_id)
-
-# get the gages not selected (n=89)
-gages_not_selected_v2 <- sel_gages_bmi %>% 
-  filter(!site_id %in% sel_bmi_coms_final_v2$site_id)
-
-# get hucs selected (n=205)
-hucs_selected_v2 <- sel_h12_bmi %>% 
-  filter(HUC_12 %in% sel_bmi_coms_final_v2$HUC_12)
-
-bmi_coms_dat %>% 
-  st_drop_geometry() %>% 
-  distinct(site_id, .keep_all=TRUE) %>% 
-  group_by(CEFF_type) %>% tally()
+# tidy streams
+# get distinct segs that are DS only (either DD or DS)
+mainstems_distinct <- mainstems_all %>% 
+  filter(from_gage %in% c("DS", "DD")) %>% 
+  distinct(nhdplus_comid, .keep_all=TRUE)
 
 # Set up Mapview Basemap --------------------------------------------------
 
@@ -63,10 +42,14 @@ mapviewOptions(basemaps=basemapsList)
 # Make Mapview of Selected Gages and BMI Stations ----------------------
 
 # this map of all sites selected U/S and D/S
-m3 <- mapview(sel_bmi_coms_final_v2, cex=6, col.regions="orange", 
-              layer.name="Selected BMI comids") +  
-  mapview(mainstems_all, color="steelblue", cex=3, 
-          layer.name="NHD Flowlines") +
+mapviewOptions(fgb = FALSE)
+
+m3 <- mapview(bmi_final_dat, cex=6, col.regions="orange", 
+              layer.name="Selected BMI CSCI") +  
+  mapview(mainstems_all %>% filter(from_gage=="UM"), color="forestgreen", cex=3, 
+          layer.name="NHD Flowlines US") +
+  mapview(mainstems_distinct, color="steelblue", cex=3, 
+          layer.name="NHD Flowlines DS") +
   mapview(gages_selected_v2, col.regions="skyblue", cex=7, color="blue2",
           layer.name="Selected USGS Gages") + 
   # these are all bmi or gages in same H12 but not selected
@@ -74,10 +57,16 @@ m3 <- mapview(sel_bmi_coms_final_v2, cex=6, col.regions="orange",
           cex=3.2, layer.name="Other USGS Gages") + 
   mapview(bmi_not_selected_v2, col.regions="gold", color="gray20", cex=3.2, 
           layer.name="Other BMI Sites in H12") + 
-  mapview(sel_h12_bmi, col.regions="dodgerblue", alpha.region=0.1, 
-          color="darkblue", legend=F, layer.name="HUC12")
+  mapview(hucs_selected_v2, col.regions="orange3", alpha.region=0.1, 
+          color="orange", legend=F, layer.name="Selected HUC12") +
+  mapview(hucs_not_selected_v2, col.regions="dodgerblue", alpha.region=0.1, 
+          color="darkblue", legend=F, layer.name="Other HUC12")
+
 
 m3@map %>% leaflet::addMeasure(primaryLengthUnit = "meters")
+
+# save out
+#mapshot(m3, url = paste0(here::here(),"/figs/03_map_of_final_bmi_csci_sites.html"))
 
 
 # TMAP MAPS ---------------------------------------------------------------
@@ -85,7 +74,7 @@ m3@map %>% leaflet::addMeasure(primaryLengthUnit = "meters")
 library(tmap)
 library(USAboundaries)
 ca<-us_counties(states="ca")
-load("data_output/major_rivers_dissolved.rda")
+load("data/spatial/major_rivers_dissolved.rda")
 
 # crop rivers to CA only and filter to rivers only
 rivs_ca <- st_intersection(rivs, ca) %>% 
@@ -99,8 +88,8 @@ rivs_ca <- st_intersection(rivs, ca) %>%
 
 # BMI stations TMAP --------------------------------------------------------------
 
-# get ALL bug data (disinct stations)
-load("data_output/00_bmi_stations_distinct.rda")
+# get ALL bug data (distinct stations)
+load("data_output/01_bmi_stations_distinct.rda")
 
 # make a tmap
 (map_bmi <- map_ca +
@@ -112,7 +101,7 @@ load("data_output/00_bmi_stations_distinct.rda")
     tm_scale_bar(breaks = c(0, 100, 200), 
                  text.size = 0.6,
                  position = c(0.12, 0.1)) +
-    tm_layout(title = "BMI Sampling\nLocations \n (n=2,935)", 
+    tm_layout(title = glue("BMI Sampling\nLocations\n (n={nrow(bmi_stations_distinct)})"), 
               legend.show = FALSE, frame = FALSE, title.size = 0.8,
               legend.outside = FALSE, attr.outside = FALSE,
               inner.margins = 0.01, outer.margins = (0.01),
@@ -121,7 +110,7 @@ load("data_output/00_bmi_stations_distinct.rda")
 
 # FIRST TRYPTYCH
 tmap::tmap_save(tm = map_bmi, 
-                filename = "figs/04_tmap_bmi_sites_all.png", width = 8, height = 11, units = "in", dpi = 300)  
+                filename = "figs/03_tmap_bmi_sites_all.png", width = 8, height = 11, units = "in", dpi = 300)  
 
 
 # GAGES MAP ---------------------------------------------------------------
@@ -140,7 +129,7 @@ usgs_gages <- bind_rows(ref_gages, alt_gages) %>%
     tm_symbols(col= "steelblue",  border.col = "black",
                size=0.1, border.alpha = 0.8) +
     # tm_logo(file = "figs/logo_cws_websafe.png", position = c("left","bottom"), height = 5) +
-    tm_layout(title = "USGS Sites \n(n=2,316)", 
+    tm_layout(title = "USGS Sites \n(n=2316)", 
               fontfamily = "Roboto Condensed",title.size = 0.8,
               legend.outside = FALSE, attr.outside = FALSE,
               inner.margins = 0.01, outer.margins = (0.01),
@@ -148,7 +137,7 @@ usgs_gages <- bind_rows(ref_gages, alt_gages) %>%
 
 
 tmap::tmap_save(tm = map_usgs, 
-                filename = "figs/04_tmap_usgs_sites_all.png", width = 8, height = 11, units = "in", dpi = 300)  
+                filename = "figs/03_tmap_usgs_sites_all.png", width = 8, height = 11, units = "in", dpi = 300)  
 
 # FINAL SITES (SELECTED GAGES ONLY) TMAP -------------------------------------------
 
@@ -156,7 +145,9 @@ tmap::tmap_save(tm = map_usgs,
 (map_final_sites <- map_ca +
    tm_shape(gages_selected_v2) +
    tm_dots(col="#21908CFF", shape=21, size=0.3, alpha=1) +
-   tm_layout(title = "Selected Sites \n(USGS Gages)",
+   tm_shape(bmi_final_dat) +
+   tm_dots(col="orange", shape=21, size=0.1, alpha=1) +
+   tm_layout(title = "Selected Sites:\nGages (n=226)\nBMI (n=275)",
              title.size = 0.8, frame = FALSE, 
              fontfamily = "Roboto Condensed",
              legend.outside = FALSE, attr.outside = FALSE,
@@ -165,7 +156,7 @@ tmap::tmap_save(tm = map_usgs,
   #tm_compass(type = "4star", position = c("right","top"))+
   #tm_scale_bar(position = c("left","bottom"))
 
-tmap::tmap_save(tm=map_final_sites, filename = "figs/04_tmap_selected_paired_sites_w_rivers.png", width = 8, height = 11, units = "in", dpi = 300)  
+tmap::tmap_save(tm=map_final_sites, filename = "figs/03_tmap_selected_paired_sites_w_rivers.png", width = 8, height = 11, units = "in", dpi = 300)  
 
 
 # Put them all together ---------------------------------------------------
@@ -174,7 +165,7 @@ final_triptych<-tmap::tmap_arrange(map_bmi, map_usgs, map_final_sites, ncol = 3,
 print(final_triptych)
 
 tmap::tmap_save(tm = final_triptych, 
-                filename = "figs/04_tmap_triptych_bmi_usgs_selected.png", width = 11, height = 7, units = "in", dpi = 300)  
+                filename = "figs/03_tmap_triptych_bmi_usgs_selected.png", width = 11, height = 7, units = "in", dpi = 300)  
 
 
 # TMAP PALETTE EXPLORER ---------------------------------------------------
