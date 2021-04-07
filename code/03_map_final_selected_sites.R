59632b941895a476d881b72b95b20c5321aad6a5
Ryan Peek <rapeek@ucdavis.edu>
2021-04-06 17:13:25 -0700

    update lollipop plots w asci and csci

--- code/03_map_final_selected_sites.R
+++ code/03_map_final_selected_sites.R
@@ -16,9 +16,6 @@ library(tmaptools)
 
 # Load Data ---------------------------------------------------------------
 
-# FISH REGIONS
-#ca_sp_regions <- read_sf("data/spatial/umbrella_sp_regions.shp", as_tibble = T)
-
 # Selected Sites: with BMI data
 load("data_output/02c_selected_final_bmi_dat_all.rda")
 
@@ -30,6 +27,14 @@ load("data_output/02b_sel_gage_mainstems_all.rda")
 mainstems_distinct <- mainstems_all %>% 
   filter(from_gage %in% c("DS", "DD")) %>% 
   distinct(nhdplus_comid, .keep_all=TRUE)
+
+# get ecoregions
+eco_revised <- read_rds("data/spatial/ecoregions_combined_L3.rds")
+
+# asci data: https://github.com/ksirving/asci_ffm_2019/tree/master/output_data
+asci <- read_rds("data_output/02c_selected_final_algae_asci_dat.rds")
+load("data_output/01a_algae_stations_distinct.rdata") # algae_stations_distinct
+asci_all <- algae_stations_distinct
 
 # Set up Mapview Basemap --------------------------------------------------
 
@@ -86,22 +91,38 @@ rivs_ca <- st_intersection(rivs, ca) %>%
   tm_shape(rivs_ca) + tm_lines(col="darkblue", lwd = .7, alpha = 0.8) +
   tm_layout(frame=FALSE))
 
+#  CA with revised ecoregions
+(map_ecoca <- tm_shape(ca) + tm_polygons(border.alpha = 0.3) +
+    tm_layout(frame=FALSE) +
+    tm_shape(eco_revised) + tm_polygons(border.alpha=0.1, col = "US_L3_mod", alpha = 0.7, palette="viridis", title="bmi_ffm_links.RprojEcoregions") +
+    tm_shape(rivs_ca) + tm_lines(col="darkblue", lwd = .5, alpha = 0.4) +
+    tm_layout(legend.width = 0.45, fontfamily = "Helvetica") +
+    tm_compass(type = "arrow", size = 2,
+               position = c(0.1,0.18)) +
+    #position = c(0.15, 0.2)) +
+    tm_scale_bar(breaks = c(0, 100, 200), 
+                 text.size = 0.6,
+                 position = c(0.12, 0.1))
+)
+tmap::tmap_save(tm = map_ecoca, 
+                filename = "figs/03_tmap_ecoregions_revised.png", width = 8, height = 11, units = "in", dpi = 300)
+
 # BMI stations TMAP --------------------------------------------------------------
 
-# get ALL bug data (distinct stations)
+# get ALL bug data (distinct stations)      
 load("data_output/01_bmi_stations_distinct.rda")
 
 # make a tmap
 (map_bmi <- map_ca +
     tm_shape(bmi_stations_distinct) +
-    tm_dots(col = "orange", shape = 21, size = 0.1, alpha=0.8) + 
+    tm_dots(col = "#FDE725FF", shape = 21, size = 0.1, alpha=0.8, border.alpha=0.9) + 
     tm_compass(type = "arrow", size = 2,
                position = c(0.1,0.18)) +
     #position = c(0.15, 0.2)) +
     tm_scale_bar(breaks = c(0, 100, 200), 
                  text.size = 0.6,
                  position = c(0.12, 0.1)) +
-    tm_layout(title = glue("BMI Sampling\nLocations\n (n={nrow(bmi_stations_distinct)})"), 
+    tm_layout(title = glue("Sampling Locations\n (CSCI: n={nrow(bmi_stations_distinct)})"), 
               legend.show = FALSE, frame = FALSE, title.size = 0.8,
               legend.outside = FALSE, attr.outside = FALSE,
               inner.margins = 0.01, outer.margins = (0.01),
@@ -110,7 +131,34 @@ load("data_output/01_bmi_stations_distinct.rda")
 
 # FIRST TRYPTYCH
 tmap::tmap_save(tm = map_bmi, 
-                filename = "figs/03_tmap_bmi_sites_all.png", width = 8, height = 11, units = "in", dpi = 300)  
+                filename = "figs/03_tmap_bio_sites_all.png", width = 8, height = 11, units = "in", dpi = 300)  
+
+
+# ASCI stations TMAP ------------------------------------------------------
+
+# make a tmap
+(map_asci <- map_ca +
+   # tm_shape(gages_selected_v2) +
+   # tm_dots(col="cyan3", shape=21, size=0.4, alpha=1) +
+   tm_shape(asci) +
+   tm_dots(col = "red2", shape = 21, size = 0.3, alpha=1, border.alpha=0.3, legend.show = TRUE) + 
+   tm_shape(bmi_final_dat) +
+   tm_dots(col="#FDE725FF", shape=21, size=0.3, alpha=0.7, border.alpha=0.3, legend.show = TRUE) +
+   tm_compass(type = "arrow", size = 2,
+              position = c(0.1,0.18)) +
+   tm_scale_bar(breaks = c(0, 100, 200), 
+                text.size = 0.6,
+                position = c(0.12, 0.1)) +
+   tm_layout(title = glue("Stations\n (ASCI: n=243\n  CSCI: n=275)"), 
+             frame = FALSE, title.size = 0.8,
+             legend.outside = FALSE, attr.outside = FALSE,
+             inner.margins = 0.01, outer.margins = (0.01),
+             fontfamily = "Roboto Condensed", title.position = c(0.65, 0.7)))
+
+
+# ASCI
+tmap::tmap_save(tm = map_bmi, 
+                filename = "figs/03_tmap_selected_bio_sites_combined.png", width = 8, height = 11, units = "in", dpi = 300)  
 
 
 # GAGES MAP ---------------------------------------------------------------
--- code/10_GBM_combine_asci_csci_metrics.R
+++ code/10_GBM_combine_asci_csci_metrics.R
@@ -18,43 +18,39 @@ load("data_output/05_bmi_csci_por_trim_ecoreg.rda")
 load("models/09_csci_por_all_ri_all_regions.rda")
 
 hydroDat <- "POR"
-plotname <- "All Site Pairs"  #"Central Valley" #"All Site Pairs"
+plotname <- "All Site Pairs"  #"All Site Pairs"
 
 ## ONLY IF YOU NEED MODEL NAMES/DATA
-## "all_ca_ffc_only", "central_valley", "great_basin", "north_coast", "south_coast", 
 modname <- "all_ca" # model name 
 bmiVar <- quote(csci) # select response var
 # make pathnames
-(plot_savename <- tolower(glue("09_gbm_", as_name(bmiVar), "_",hydroDat, "_",modname)))
+#(plot_savename <- tolower(glue("09_gbm_", as_name(bmiVar), "_",hydroDat, "_",modname)))
 
 # rename objects for brevity
-csci_por <- bmi_csci_por
-csci_por_trim <- bmi_csci_por_trim
+csci_por_trim <- bmi_csci_por_trim_ecoreg
 csci_ri_all <- ri_all_regions
 
-rm(bmi_csci_por, bmi_csci_por_trim, ri_all_regions)
+rm(bmi_csci_por_trim_ecoreg, ri_all_regions)
 
 # Load ASCI Data ----------------------------------------------------------
 
-#"04_selected_algae_asci_por_trim_w_huc_region.rda"
-#"04_selected_algae_asci_por_w_huc_region.rda"
+# this is sites
+load(url("https://github.com/ksirving/asci_ffm_2019/blob/master/output_data/05_algae_asci_por_trim_ecoreg.rda?raw=true")) 
 
-load(url("https://github.com/ksirving/asci_ffm_2019/blob/master/output_data/04_selected_algae_asci_por_trim_w_huc_region.rda?raw=true")) # IT WORKED!
-load(url("https://github.com/ksirving/asci_ffm_2019/blob/master/output_data/04_selected_algae_asci_por_w_huc_region.rda?raw=true"))
-load(url("https://github.com/ksirving/asci_ffm_2019/blob/master/models/07_all_ri_all_regions_asci.rda?raw=true"))
+# this is RI outputs
+load(url("https://github.com/ksirving/asci_ffm_2019/blob/master/models/09_h_asci.x_por_all_ri_all_regions.rda?raw=true"))
 
 # rename for brevity
-asci_por <- algae_asci_por
-asci_por_trim <- algae_asci_por_trim
+asci_por_trim <- algae_asci_por_trim_ecoreg
 asci_ri_all <- ri_all_regions
 
-rm(algae_asci_por, algae_asci_por_trim, ri_all_regions)
+rm(algae_asci_por_trim_ecoreg, ri_all_regions)
 
 
 # Save ALL data -----------------------------------------------------------
 
-save(asci_por, asci_por_trim, asci_ri_all, csci_por, csci_por_trim, csci_ri_all, 
-     file = "data_output/09C_csci_asci_ri_POR_all_regions.rda")
+save(asci_por_trim, asci_ri_all, csci_por_trim, csci_ri_all, 
+     file = "models/10_csci_asci_ri_por_trim_all_regions.rda")
 
 
 # Merge ASCI/CSCI DATA ----------------------------------------------------
@@ -64,14 +60,17 @@ asci_ri_all <- asci_ri_all %>% mutate(index="ASCI")
 csci_ri_all <- csci_ri_all %>% mutate(index="CSCI")
 
 # combine
-ri_all <- bind_rows(asci_ri_all, csci_ri_all)
+ri_all <- bind_rows(asci_ri_all, csci_ri_all) %>% 
+  # replace all H_ASCI.x with "asci"
+  mutate(Ymetric = case_when(
+    grepl("H_ASCI.x", Ymetric) ~ "asci",
+    TRUE ~ Ymetric
+  ))
 
 # Make a Table of RI's ----------------------------------------------------
 
 library(readxl)
 ff_defs <- readxl::read_xlsx("docs/Functional_Flow_Metrics_List_and_Definitions_final.xlsx", range = "A1:F25", .name_repair = "universal", trim_ws = TRUE) 
-
-# 
 
 # join with the full RI table
 ri_table <- left_join(ri_all, ff_defs, by=c("var"="Flow.Metric.Code"))
@@ -90,61 +89,70 @@ levels(ri_table$flow_component)
 summary(ri_table)
 
 # generate order by CA wide RI for flow metrics:
-forder <- ri_table %>% 
-  filter(model=="all_ca", 
+forder_csci <- ri_table %>% 
+  filter(model=="all_ca", index=="CSCI",
          method=="mse") %>% 
   mutate(model=as.factor(model),
          Flow.Metric.Name = as.factor(Flow.Metric.Name),
          Flow.Metric.Name = forcats::fct_reorder2(Flow.Metric.Name, model, RI)) %>% 
-  group_by(model, index) %>% 
+  group_by(model) %>% 
   arrange(desc(model, RI)) %>% 
   mutate(id = row_number()) %>% 
   ungroup() %>% arrange(id) %>% 
-  select(Flow.Metric.Name, id) # get the just the names for ordering things
+  select(Flow.Metric.Name,Ymetric, id) # get the just the names for ordering things
 
+forder_asci <- ri_table %>% 
+  filter(model=="all_ca", index=="ASCI",
+         method=="mse") %>% 
+  mutate(model=as.factor(model),
+         Flow.Metric.Name = as.factor(Flow.Metric.Name),
+         Flow.Metric.Name = forcats::fct_reorder2(Flow.Metric.Name, model, RI)) %>% 
+  group_by(model) %>% 
+  arrange(desc(model, RI)) %>% 
+  mutate(id = row_number()) %>% 
+  ungroup() %>% arrange(id) %>% 
+  select(Flow.Metric.Name, Ymetric, id) # get the just the names for ordering things
+
+# join these together for later
+forder_all <- full_join(forder_csci, forder_asci)
 
 # Plot & Summarize All RI Combined ----------------------------------------
 
-# most common hydrometric by flowdata type?
-ri_table %>% group_by(flowdat, var) %>% 
+# highest RI hydrometric variable across both ASCI and CSCI
+# and including all regions for Period of Record data
+ri_table %>% filter(method=="mse") %>% 
+  group_by(var) %>% # only group by hydrometric
   summarize(meanRI = mean(RI),
-            sumRI = sum(RI)) %>% 
-  top_n(5) %>% 
-  arrange(flowdat, desc(meanRI))
+            medianRI = median(RI)) %>% 
+  ungroup() %>% 
+  arrange(desc(meanRI)) %>% 
+  top_n(10)
 
-# so best hydrometric across model?
-ri_table %>% group_by(model, index, var) %>% 
+# so most consistent hydrometric including all_ca + regional models in each ASCI and CSCI
+ri_table %>% filter(method=="mse") %>% 
+  group_by(index, var) %>% 
   summarize(meanRI = mean(RI),
-            medianRI = median(RI),
-            maxRI = max(RI),
-            SD = sd(RI)) %>% 
-  top_n(5) %>% group_by(var, index) %>% tally() %>% arrange(desc(index, n)) #%>% 
-  #View()
-
-# top metrics based on frequency across regions
-# Wet_BFL_Mag_50     5
-# DS_Mag_50          3
-# DS_Tim             3
-# Peak_5             3
-# SP_ROC             3
-# DS_Dur_WS          2
-
+            medianRI = median(RI)) %>% 
+  top_n(10) %>% 
+  arrange(index, desc(meanRI)) 
+  
 
 # Summary Table ALL CA -----------------------------------------------------------
 
 library(glue)
 
 model_name <- "All CA"
+
 ## ALL CA Table
 # Create a gt table based on preprocessed table
 ri_table %>%
   dplyr::filter(model=="all_ca", method=="mse") %>%
   dplyr::select(-c(Ymetric, flowdat, flow_component, method, model, Flow.Characteristic)) %>%
-  dplyr::select(Flow.Component, var, Flow.Metric.Name:Flow.Metric.Description, RI) %>%
-  arrange(Flow.Component, var) %>% #View() 
+  dplyr::select(index, Flow.Component, var, Flow.Metric.Name:Flow.Metric.Description, RI) %>%
+  arrange(desc(RI), Flow.Component, var) %>% #View() 
   gt() %>%
   tab_header(
-    title = "Relative Influence of Functional Flow Metrics on CSCI",
+    title = "Relative Influence of Functional Flow Metrics on ASCI/CSCI",
     subtitle = glue::glue("Model {model_name}")
   ) %>%
   fmt_number(
@@ -154,24 +162,26 @@ ri_table %>%
 
 # Summary Table Regional -----------------------------------------------------------
 
-model_name <- "HUC Regions"
+model_name <- "modified Ecoregions"
 
 # Create a gt table based on preprocessed table
 ri_table %>%
   dplyr::filter(!model=="all_ca", method=="mse") %>%
   dplyr::select(-c(Ymetric, flowdat, flow_component, method, Flow.Characteristic)) %>%
   pivot_wider(names_from = model, values_from = RI) %>% # View()
-  dplyr::select(Flow.Component, var, Flow.Metric.Name:Unit, great_basin:south_coast) %>%
+  dplyr::select(Flow.Component, var, Flow.Metric.Name:Unit, so_cal:north_coast) %>%
   arrange(Flow.Component, var) %>% # View() 
   gt() %>%
   tab_header(
-    title = "Relative Influence of Functional Flow Metrics on CSCI",
-    subtitle = glue::glue("Model {model_name}")
+    title = "Relative Influence of Functional Flow Metrics on ASCI/CSCI",
+    subtitle = glue::glue("Using {model_name}")
   ) %>%
   fmt_number(
-    columns = vars(central_valley, north_coast, south_coast, great_basin), decimals = 1, 
+    columns = vars(so_cal, cent_coast, sierras, north_coast), decimals = 1, 
     drop_trailing_zeros = T
   )
+
+
 
 # Summary Plot ALL CA ------------------------------------------------------------
 
@@ -207,8 +217,8 @@ ri_table %>%
   scale_size_area("", guide=FALSE) +
   #scale_shape_manual("Method", values=c("mse"=16, "permtest"=17))+
   coord_flip() +
-  ylim(c(0,25))+
-  labs(title = glue::glue('{plotname}: {hydroDat}'),
+  ylim(c(0,20))+
+  labs(title = glue::glue('All Site Pairs (CA)'),
        x="", y="Relative Influence (%)") +
   # fix legend
   guides(color = guide_legend(override.aes = list(size = 4, pch=21, 
@@ -222,46 +232,53 @@ ri_table %>%
         legend.background = element_rect(color="white"))
 
 # save out
-ggsave(filename=tolower(paste0("models/", plot_savename, "_all_ri_sized_points_w_lines_ranked.png")), width = 9, height = 7, units = "in", dpi = 300)
+ggsave(filename=tolower(glue("models/10_gbm_asci_csci_{hydroDat}_{modname}_ri_sized_points_w_lines_ranked.png")), width = 9, height = 7, units = "in", dpi = 300)
 
 # Summary Plot Regions ------------------------------------------------------------
 
 # now plot w facets (but use same ordering for ALL CA)
 ri_table %>% 
-  filter(#model!="all_ca", 
-         model=="all_ca", 
+  filter(model!="all_ca",
          method=="mse") %>% 
-  left_join(., forder, by="Flow.Metric.Name") %>% 
+  left_join(., forder_asci, by="Flow.Metric.Name") %>% 
   arrange(id) %>% #View() 
   ggplot() +
-  facet_grid(cols = vars(model), labeller = labeller(model=c("all_ca"="All CA", "central_valley"="Central Valley", "great_basin"="Great Basin", "north_coast"="N. Coast", "south_coast"="S. Coast"))) +
+  facet_grid(cols = vars(model), labeller = labeller(model=c("all_ca"="All CA", "cent_coast"="C. Coast", "sierras"="Sierra Nevada", "north_coast"="N. Coast", "so_cal"="S. California"))) +
   geom_linerange(aes(x=reorder(Flow.Metric.Name, desc(id)), ymax=RI, ymin=0, color=flow_component, group=model), 
                   lwd=.5, show.legend = F, alpha=0.7, lty=1)+
-  geom_point(aes(x=reorder(Flow.Metric.Name, desc(id)), y=RI, fill=flow_component, size=RI, group=model), 
-             #size=4, 
-             show.legend = TRUE, pch=21) +
-  #scale_x_continuous(breaks=forder$id, labels=forder$Flow.Metric.Name) +
+  geom_point(aes(x=reorder(Flow.Metric.Name, desc(id)), y=RI,
+                 fill=flow_component, size=RI, group=model, shape=index),
+             color="black", alpha=0.8) +
   scale_fill_manual("Flow Component", values=flowcomponent_colors) +
-  scale_color_manual("Flow Component", values=flowcomponent_colors) +
-  scale_size_binned("", guide=FALSE, range=c(0.5, 6.5)) +
+  scale_color_manual("Flow Component", values=flowcomponent_colors, guide=FALSE) +
+  scale_shape_manual("Index", values=c("ASCI"=23, "CSCI"=21))+
+  scale_size_area("", guide=FALSE) +
   coord_flip() +
-  ylim(c(0,30))+
-  labs(subtitle = "CSCI Models",
+  ylim(c(0,27))+
+  labs(title = "ASCI & CSCI BRT Models",
        x="", y="Relative Influence (%)") +
-  #theme_bw(base_family = "Roboto Condensed")
   theme_minimal(base_family = "Roboto Condensed") +
-  theme(legend.position = "bottom", legend.box = "horizontal")+
-  guides(fill = guide_legend(override.aes = list(size = 4), 
-                             title.position = "top"))
-  #facet_grid(cols = vars(model), labeller = labeller(model=c("central_valley"="Central Valley", "great_basin"="Great Basin", "north_coast"="N. Coast", "south_coast"="S. Coast")))
+  theme(legend.position = "bottom", 
+        legend.box="vertical",
+        legend.spacing.y = unit(-0.1,"cm"),
+        legend.justification = "center",
+        legend.margin = margin(6,0,6, 0),
+        legend.box.spacing = unit(-.01,"cm"),
+        legend.background = element_rect(color="white")) +
+  guides(fill = guide_legend(override.aes = 
+                               list(size = 4, pch=21, fill=flowcomponent_colors, 
+                                    color="gray40"), nrow = 1, order = 1, direction="horizontal"),
+         shape = guide_legend(override.aes = list(size = 4,
+                                                  direction="horizontal"), order = 2),
+         size= "none")
 
 # save out
-plot_savename2 <- "09_gbm_csci_por_regional"
-ggsave(filename=tolower(paste0("models/", plot_savename2, "_ri_points_w_lines_ranked.png")), width = 9, height = 7, units = "in", dpi = 300)
 
+# WITH CA
+ggsave(filename=tolower(glue("models/10_gbm_asci_csci_{hydroDat}_ca_and_regions_ri_sized_points_w_lines_ranked.png")), width = 9, height = 7, units = "in", dpi = 300)
 
-# all 5
-ggsave(filename=tolower(paste0("models/", plot_savename2, "_all_regions_ri_points_w_lines_ranked.png")), width = 9, height = 7, units = "in", dpi = 300)
+# REGIONS ONLY
+ggsave(filename=tolower(glue("models/10_gbm_asci_csci_{hydroDat}_regions_ri_sized_points_w_lines_ranked.png")), width = 9, height = 7, units = "in", dpi = 300)
 
 
 # Other Plots -------------------------------------------------------------
