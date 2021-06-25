93e406c73f6702112278d7b8044abead5ea068a7
Ryan Peek <rapeek@ucdavis.edu>
2021-06-25 15:10:49 -0700

    update with proportion filtering of FFM

--- code/06_ffm_evaluation.R
+++ code/06_ffm_evaluation.R
@@ -1,4 +1,7 @@
 # FFM Evaluation
+# Filter Ref gages to assess best FF metrics
+# to refine metrics that need to be run
+# R. Peek 2021
 
 
 # Libraries ---------------------------------------------------------------
@@ -8,9 +11,9 @@ library(glue)
 options(tidyverse.quiet = TRUE)
 library(tidyverse) # load quietly
 library(viridis) # colors
+library(janitor)
 
-
-# Data --------------------------------------------------------------------
+# GET DATA ----------------------------------------------------------------
 
 # REF gages
 gages_ref <- read_csv("data/usgs/gages_ref_223_period_record.csv") %>% 
@@ -19,15 +22,23 @@ gages_ref <- read_csv("data/usgs/gages_ref_223_period_record.csv") %>%
   mutate(refgage="Ref")
 
 # ffc data (alteration status)
-ffc_dat <- read_rds(file = url("https://github.com/ryanpeek/ffm_comparison/raw/main/output/ffc_combined/usgs_combined_alteration.rds")) %>%
+ffc_alt <- read_rds(file = url("https://github.com/ryanpeek/ffm_comparison/raw/main/output/ffc_combined/usgs_combined_alteration.rds")) %>%
   mutate(gage=as.numeric(gageid),
          status=as.factor(status),
          alteration_type=as.factor(alteration_type))
 
-# Tidy and Clean ----------------------------------------------------------
+# import observed FFM data for period of record
+ffc_obs <- read_rds(file = url("https://github.com/ryanpeek/ffm_comparison/raw/main/output/ffc_combined/usgs_combined_ffc_percentiles.rds")) %>%
+  mutate(gage=as.numeric(gageid))
+
+# import predicted percentiles for period of record
+ffc_pred <- read_rds(file=url("https://github.com/ryanpeek/ffm_comparison/raw/main/output/ffc_combined/usgs_combined_predicted_percentiles.rds")) %>% 
+  mutate(gage=as.numeric(gageid))
+
+# TIDY AND CLEAN ----------------------------------------------------------
 
 # get distinct gages (n=959)
-ffc_gages <- ffc_dat %>% distinct(gageid, .keep_all=TRUE) %>% 
+ffc_gages <- ffc_alt %>% distinct(gageid, .keep_all=TRUE) %>% 
   mutate(gage=as.numeric(gageid)) %>% 
   left_join(., gages_ref, by="gage") %>% 
   mutate(refgage = case_when(
@@ -36,42 +47,42 @@ ffc_gages <- ffc_dat %>% distinct(gageid, .keep_all=TRUE) %>%
   select(-c(metric:median_in_iqr))
 
 # join FFC_DAT w ref gages (FROM CEFF/FFM) data to get REF/non-ref class
-ffc_dat <- left_join(ffc_dat, gages_ref, by="gage") %>% 
+ffc_alt <- left_join(ffc_alt, gages_ref, by="gage") %>% 
   mutate(refgage = case_when(
     is.na(refgage) ~ "Non-Ref",
     TRUE ~ refgage))
 
 # tally (24 metrics x 959 gages = 23016)
-ffc_dat %>% group_by(refgage, status) %>%
+ffc_alt %>% group_by(refgage, status) %>%
   tally(name="total_status") %>% # tally by status
   # tally by ref gage class
   add_tally(total_status, name="total_of_refgage_class") # so only 21912, some NA's still
 
-summary(ffc_dat$status)
-summary(ffc_dat$status_code)
+summary(ffc_alt$status)
+summary(ffc_alt$status_code)
 ## so not_enough_data = NA in status_code
-summary(ffc_dat$alteration_type)
+summary(ffc_alt$alteration_type)
 
 
-# Calculate by REF/NON-REF ------------------------------------------
+# CALC PROPORTION GAGES ALTERED PER METRIC ----------------
 
 # count total records by metric & refgage
-(ffc_metric_count <- ffc_dat %>% 
+(ffm_metric_count <- ffc_alt %>% 
    group_by(metric, refgage) %>% 
    count(name = "total_count"))
 
 # calculate proportion of gages/total for each status class
-ffc_prcnt_alt <- ffc_dat %>% 
+ffm_prcnt_alt <- ffc_alt %>% 
   group_by(metric, status, refgage) %>% # for facet by refgage
   tally() %>% 
-  left_join(., ffc_metric_count) %>% 
+  left_join(., ffm_metric_count) %>% 
   mutate(prop_n = n/total_count)
 
 
-## Plots: ALT STATUS -------------------------------------------------------
+## Plot: ALT STATUS -------------------------------------------------------
 
 # plot faceted by REF/NON-REF type
-ggplot() + geom_col(data=ffc_prcnt_alt, aes(x=metric, y=prop_n, fill=status)) +
+ggplot() + geom_col(data=ffm_prcnt_alt, aes(x=metric, y=prop_n, fill=status)) +
   theme_classic(base_family = "Roboto Condensed") +
   scale_y_continuous(labels=percent) +
   scale_fill_viridis_d(direction = -1) +
@@ -81,23 +92,66 @@ ggplot() + geom_col(data=ffc_prcnt_alt, aes(x=metric, y=prop_n, fill=status)) +
   theme(axis.text.x = element_text(angle=270, hjust = 0.1, vjust=0.05)) +
   facet_wrap(vars(refgage))
 
-ggsave(filename = "figs/prop_gages_by_alt_status_faceted.png", width = 11, height = 8.5, dpi=300)
+#ggsave(filename = "figs/prop_gages_by_alt_status_faceted.png", width = 11, height = 8.5, dpi=300)
 
-# Calculate REF/NON-REF w medianIQR ------------------------------
+# FILTERING FFM ---------------------------------------------
 
-summary(ffc_dat$median_in_iqr)
+## STEP 1: <10% of Ref gages altered ------------------------
+
+# Filter to only FFM with < 10% of ref gages == altered, excluding PEAK FLOW METRICS 
+
+ffm_prcnt_alt_keep <- ffm_prcnt_alt %>% 
+  filter(refgage == "Ref") %>% # keep only REF
+  filter(status == "likely_altered") %>% 
+  filter(prop_n < 0.1) %>% 
+  filter(!grepl("^Peak", metric))
+
+# FILTER 1 METRIC LIST
+filt_less_than_10_perc_metrics <- ffm_prcnt_alt_keep$metric
+
+## STEP 2: <15% of Ref gages peak flows altered -------------
+
+# Filter to only Peak flow metris w < 15% of ref gages == altered
+ffm_prcnt_alt_keep_peak <- ffm_prcnt_alt %>% 
+  filter(refgage == "Ref") %>% # keep only REF
+  filter(status == "likely_altered") %>% 
+  filter(grepl("^Peak", metric)) %>% 
+  filter(prop_n < 0.15)
+
+# FILTER 1 METRIC LIST
+filt_less_than_15_peak <- ffm_prcnt_alt_keep_peak$metric
+
+## STEP 3: <25% of Ref gages are indeterminant -----------
+
+# <25% of ref gages are indeterminate for that metric
+
+ffm_prcnt_alt_keep_indet <- ffm_prcnt_alt %>% 
+  filter(refgage == "Ref") %>% # keep only REF
+  filter(status == "indeterminate") %>% 
+  filter(prop_n < 0.25)
+
+# FILTER 1 METRIC LIST
+filt_less_than_25_indeterm <- ffm_prcnt_alt_keep_indet$metric
+
+### WRITE IT OUT
+write_csv(ffm_prcnt_alt_keep_indet, file = "data_output/06_filtered_ffm_based_on_ref_prop.csv")
+
+# ADD PLOTS/SUMMARIES ---------------------------------------
+# Calc REF/NON-REF w medianIQR ------------------------------
+
+summary(ffc_alt$median_in_iqr)
 
 # calculate proportion of gages/total
-ffc_prcnt_alt2 <- ffc_dat %>% 
+ffm_prcnt_alt2 <- ffc_alt %>% 
   group_by(metric, median_in_iqr, refgage) %>% # for facet by refgage
   tally() %>% 
-  left_join(., ffc_metric_count) %>% 
+  left_join(., ffm_metric_count) %>% 
   mutate(prop_n = n/total_count)
 
-## Plots: by median IQR ---------------------------------------------------
+## Plot: by median IQR ---------------------------------------------------
 
 # plot faceted by REF/NON-REF type
-ggplot() + geom_col(data=ffc_prcnt_alt2, aes(x=metric, y=prop_n, fill=median_in_iqr)) +
+ggplot() + geom_col(data=ffm_prcnt_alt2, aes(x=metric, y=prop_n, fill=median_in_iqr)) +
   theme_classic(base_family = "Roboto Condensed") +
   scale_y_continuous(labels=percent) +
   scale_fill_viridis_d(direction = -1, na.value="gray50") +
@@ -107,15 +161,15 @@ ggplot() + geom_col(data=ffc_prcnt_alt2, aes(x=metric, y=prop_n, fill=median_in_
   theme(axis.text.x = element_text(angle=270, hjust = 0.1, vjust=0.05)) +
   facet_wrap(vars(refgage))
 
-ggsave(filename = "figs/prop_gages_by_medIQR_faceted.png", width = 11, height = 8.5, dpi=300)
+# ggsave(filename = "figs/prop_gages_by_medIQR_faceted.png", width = 11, height = 8.5, dpi=300)
 
-# Calculate REF/NON-REF by ALT type ------------------------------
+# Calc REF/NON-REF by ALT type ------------------------------
 
 # calculate proportion of gages/total for each alt type
-ffc_prcnt_alt3 <- ffc_dat %>% 
+ffm_prcnt_alt3 <- ffc_alt %>% 
   group_by(metric, alteration_type, refgage) %>% # for facet by refgage
   tally() %>% 
-  left_join(., ffc_metric_count) %>% 
+  left_join(., ffm_metric_count) %>% 
   mutate(prop_n = n/total_count,
          # fix order of alt_type
          alteration_type = forcats::fct_relevel(alteration_type, "high", "low", 
@@ -125,9 +179,8 @@ ffc_prcnt_alt3 <- ffc_dat %>%
 
 ## Plots: by Alt Type ---------------------------------------------------
 
-
 # plot faceted by REF/NON-REF type
-ggplot() + geom_col(data=ffc_prcnt_alt3, aes(x=metric, y=prop_n, fill=alteration_type), alpha=0.8) +
+ggplot() + geom_col(data=ffm_prcnt_alt3, aes(x=metric, y=prop_n, fill=alteration_type), alpha=0.8) +
   theme_classic(base_family = "Roboto Condensed") +
   scale_y_continuous(labels=percent) +
   scale_fill_manual(values=c("high"="#E41A1C", "low"="#377EB8", "early"="#4DAF4A", "late"="#984EA3", "none_found"="gray80", "undeterminable"="gray40","unknown"="gray20")) +
@@ -137,5 +190,5 @@ ggplot() + geom_col(data=ffc_prcnt_alt3, aes(x=metric, y=prop_n, fill=alteration
   theme(axis.text.x = element_text(angle=270, hjust = 0.1, vjust=0.05)) +
   facet_wrap(vars(refgage))
 
-ggsave(filename = "figs/prop_gages_by_alt_type_faceted.png", width = 11, height = 8.5, dpi=300)
+# ggsave(filename = "figs/prop_gages_by_alt_type_faceted.png", width = 11, height = 8.5, dpi=300)
 
